1e751fe93da8417a279ca8ca9a756c8c8f83f9f5
Leia Quesada <leiaquesada@gmail.com>
2021-03-23 19:53:43 -0700

    Connect database to express

--- server/index.js
+++ server/index.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const port = 5000;
 
-// json body parser
-app.use(express.json());
 // allows cross origin requests
 app.use(cors());
+// json body parser
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Ok express is connected");
+// connects to PostgreSQL
+const pgp = require("pg-promise")({});
+const cn = {
+  host: "localhost",
+  port: 5432,
+  database: "sightings",
+  user: "leiaque",
+  password: "",
+};
+const db = pgp(cn);
+
+// check database connection
+app.get("/sightings", async (req, res) => {
+  try {
+    const sighting = await db.any("SELECT * FROM sighting;", [true]);
+    res.json({ sighting });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-app.listen(3001);
+app.listen(port, () => {
+  console.log(`PG-Promise & Express Listening at http://localhost:${port}`);
+});
